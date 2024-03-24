80244c1d78af2ac0af4b71a11065e8429abf7a5f
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-03-24 20:10:41 +0500

    Add files via upload

---
+++ 25_Alien_Colour #1/alien.js
@@ -0,0 +1,5 @@
+"use strict";
+let alien_color = 'green';
+if (alien_color == 'green') {
+    console.log("Congratulations! You just earned 5 points.");
+}
---
+++ 25_Alien_Colour #1/alien.ts
@@ -0,0 +1,15 @@
+// First version of this program.
+ 
+let alien_color = 'green'
+
+if (alien_color == 'green') {
+    console.log("Congratulations! You just earned 5 points.")
+}
+
+// Second version
+
+alien_color = 'blue'
+
+if (alien_color == 'green') {
+    console.log("Congratulations! You just earned 5 points.")
+}
