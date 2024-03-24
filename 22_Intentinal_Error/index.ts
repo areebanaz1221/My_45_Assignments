924e2843749b0c1ebd16cc1b63629931f07d034a
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-03-24 20:06:21 +0500

    Add files via upload

---
+++ 22_Intentinal_Error/index.ts
@@ -0,0 +1,27 @@
+// Function to create a fruit object.
+function createFruit(name: string, color: string, taste: string) {
+    return {
+        name,
+        color,
+        taste,
+    };
+}
+
+// Create an array of fruits
+const fruits = [
+    createFruit("Apple", "Red", "Sweet"),
+    createFruit("Banana", "Yellow", "Sweet"),
+    createFruit("Orange", "Orange", "Citrusy"),
+    createFruit("Grapes", "Purple/Green", "Sweet"),
+    createFruit("Straeberry", "Red", "Sweet"),
+];
+
+// Access an invalid index
+const invalidIndex = 10; // There are only 5 elements in the array, so this will cause an error
+console.log
+(`Fruit at index ${invalidIndex}:`, fruits [invalidIndex]);
+
+// Print the fruits
+fruits.forEach((fruit) => {
+    console.log(`Name: ${fruit.name}, Color: ${fruit.color}, Taste: ${fruit.taste}`);
+});
