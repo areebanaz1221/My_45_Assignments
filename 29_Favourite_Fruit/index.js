2fb3c2dc42c2cbf664619fc4c31f603234d9ea09
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-03-24 20:19:07 +0500

    Add files via upload

---
+++ 29_Favourite_Fruit/index.js
@@ -0,0 +1,20 @@
+"use strict";
+// Define the array of fruits
+const favourite_fruits = ["apples", "banana", "orange"];
+/// syntex: arrayName.includes("element")
+// Check for specific fruits using independent if statements
+if (favourite_fruits.includes("apple")) {
+    console.log("I like apples!");
+}
+if (favourite_fruits.includes("banana")) {
+    console.log("I enjoy bananas!");
+}
+if (favourite_fruits.includes("orange")) {
+    console.log("Orangee are delicious!");
+}
+if (favourite_fruits.includes("grapes")) {
+    console.log("Grapes are tasty!");
+}
+if (favourite_fruits.includes("mango")) {
+    console.log("Mangoes are one of my favourites!");
+}
---
+++ 29_Favourite_Fruit/index.ts
@@ -0,0 +1,24 @@
+// Define the array of fruits
+const  favourite_fruits: string[] = ["apples", "banana", "orange"];
+/// syntex: arrayName.includes("element")
+// Check for specific fruits using independent if statements
+
+if (favourite_fruits. includes("apple")) {
+    console.log("I like apples!");
+}
+
+if (favourite_fruits. includes("banana")) {
+    console.log("I enjoy bananas!");
+}
+
+if (favourite_fruits. includes("orange")) {
+    console.log("Orangee are delicious!");
+}
+
+if (favourite_fruits. includes("grapes")) {
+    console.log("Grapes are tasty!");
+}
+
+if (favourite_fruits. includes("mango")) {
+    console.log("Mangoes are one of my favourites!");
+}
