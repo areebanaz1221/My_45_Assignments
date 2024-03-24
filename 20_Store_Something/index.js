efe88f0e0bbe349e32c456cf21b3ad81bf451229
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-03-24 20:03:58 +0500

    Add files via upload

---
+++ 20_Store_Something/index.js
@@ -0,0 +1,8 @@
+"use strict";
+//Written by: Areeba Ashraf
+//DATE: 16-2-24
+const countries = ["America", "Pakistan", "Japan", "China"];
+console.log("List of Countries:");
+for (let country of countries) {
+    console.log(country);
+}
---
+++ 20_Store_Something/index.ts
@@ -0,0 +1,9 @@
+
+const countries: string[] = ["America", "Pakistan", "Japan", "China"];
+
+console.log("List of Countries:")
+
+for (let country of countries)
+{
+    console.log(country)
+}
