c74a1051dcd8a8eecacc707634f3a97f0f57b272
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-03-24 19:52:27 +0500

    Add files via upload

---
+++ 14_Guest_List/Guest_List.ts
@@ -0,0 +1,9 @@
+
+
+let Guest_List :string[] = ['Sir Kamran Khan Tessori','Sir Zia Khan','Sir Daniyal Nagori'];
+
+for(let i=0; i<Guest_List.length; i++)
+{
+    console.log('Dear Sir. ' + Guest_List[i] + ',\n\nIt is our pleasure to invite you in our party.\n\nThank You!')
+    
+}
