a100a1abdb5d0a7564583eb7bfd4a41e418a5bc9
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-03-24 20:13:40 +0500

    Add files via upload

---
+++ 26_Alien_Colour #3/alien.ts
@@ -0,0 +1,44 @@
+// 1st version
+console.log(`1st version where alien color is green`)
+let alien_color: string = 'green' ;
+
+if (alien_color === 'green')
+{
+    console.log('Congratulations! You just earned 5 points.');
+}
+ else if (alien_color === 'yellow')
+{
+    console.log('Congratulations! You just earned 10 points.');
+}
+ else if (alien_color === 'red') {
+    console.log('Congratulations! You just earned 15 points.');
+}
+ else 
+{
+    console.log('Unknown alien color.');
+}
+
+// 2nd version
+console.log(`2nd version where alien color is yellow`)
+
+alien_color = 'yellow';
+
+if (alien_color === 'green')
+{
+    console.log('Congratulations! You just earned 5 points.');
+}
+ else if (alien_color === 'yellow')
+{
+    console.log('Congratulations! You just earned 10 points.');
+}
+ else if (alien_color === 'red') {
+    console.log('Congratulations! You just earned 15 points.');
+}
+ else 
+{
+    console.log('Unknown alien color.');
+}
+
+// 3rd where alien color is red
+console.log(`where alien color is red`)
+alien_color='red';
