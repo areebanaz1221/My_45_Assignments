6f907e16912a080f9a29cada98fd7c99e92bce04
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-03-24 19:46:45 +0500

    Add files via upload

---
+++ 12_Greetings/Names.js
@@ -0,0 +1,6 @@
+"use strict";
+let names = ['Iqra', 'Arooba', 'Natasha', 'Zabunisa'];
+const message = 'Always be happy. ';
+for (let i = 0; i < names.length; i++) {
+    console.log(message + names[i]);
+}
---
+++ 12_Greetings/Names.ts
@@ -0,0 +1,11 @@
+
+
+let names :string[] = ['Iqra','Arooba','Natasha','Zabunisa'] ;
+
+const message :string = 'Always be happy. '
+
+for(let i=0; i<names.length; i++){
+
+    console.log(message + names[i]) ;
+    
+}
