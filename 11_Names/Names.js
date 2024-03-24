8397fcc9e5dc5a736275bf0c9aeaa795d8bae57f
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-03-24 19:45:08 +0500

    Add files via upload

---
+++ 11_Names/Names.js
@@ -0,0 +1,5 @@
+"use strict";
+let names = ['Iqra', 'Arooba', 'Natasha', 'Zabunisa'];
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
---
+++ 11_Names/Names.ts
@@ -0,0 +1,9 @@
+
+
+let names :string[] = ['Rabia','Khansa','Areeba'] ;
+
+for(let i=0; i<names.length; i++){
+
+    console.log(names[i]);
+    
+}
