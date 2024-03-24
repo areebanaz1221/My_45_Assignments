29c1ebc6a8f2bf3d9e269c6ebdee3c8398c69695
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-03-24 19:48:57 +0500

    Add files via upload

---
+++ 13_Favourite_Transportation/vehicles.js
@@ -0,0 +1,6 @@
+"use strict";
+let favouriteTransportation = [];
+favouriteTransportation.push(["motorcycle", "Honda"]);
+favouriteTransportation.push(["car", "Toyota"]);
+favouriteTransportation.push(["bicycle", "Sohrab"]);
+console.log(favouriteTransportation);
---
+++ 13_Favourite_Transportation/vehicles.ts
@@ -0,0 +1,10 @@
+
+
+let favouriteTransportation: Array<[transport: string, brand:
+ string]> = []
+
+ favouriteTransportation.push(["motorcycle", "Honda"])
+ favouriteTransportation.push(["car", "Toyota"])
+ favouriteTransportation.push(["bicycle", "Sohrab"])
+
+ console.log(favouriteTransportation)
