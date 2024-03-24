31cb10d9316c2197dba23d6ee30aa1f0a2ccb5ae
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-03-24 19:38:36 +0500

    Add files via upload

---
+++ 09_Favourite_Number/Favourite_Number.js
@@ -0,0 +1,4 @@
+"use strict";
+const favourite_Number = 7;
+const message = 'Here is your favourite number ' + favourite_Number;
+console.log(message);
---
+++ 09_Favourite_Number/Favourite_Number.ts
@@ -0,0 +1,7 @@
+
+
+const favourite_Number :number = 7 ;
+
+const message :string = 'Here is your favourite number ' + favourite_Number;
+
+console.log(message) ;
