dbe101a912774bd71bf2673cb29104d41b911731
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-03-24 19:25:26 +0500

    Add files via upload

---
+++ 06_Stripping_Name/Stripping_Names.js
@@ -0,0 +1,5 @@
+"use strict";
+const personName = '    \n\tAreeba Ashraf\t\n   ';
+console.log(personName);
+const without_whitespace = personName.trim();
+console.log(without_whitespace);
---
+++ 06_Stripping_Name/Stripping_Names.ts
@@ -0,0 +1,9 @@
+
+
+const personName :string = '    \n\tAreeba Ashraf\t\n   ';
+
+console.log(personName);
+
+const without_whitespace :string = personName.trim();
+
+console.log(without_whitespace);
