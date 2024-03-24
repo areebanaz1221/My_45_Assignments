bd75e23967bd00f8e8170cd038db13d495a613c8
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-03-24 20:05:14 +0500

    Add files via upload

---
+++ 21_Typescript_Objects/Objects.ts
@@ -0,0 +1,56 @@
+import { title } from "process";
+
+const bouquet = 
+{
+    name: "Spring Delight",
+    price: 250,
+    description: "Beautiful arrangment of spring flowers"
+}
+
+// define an array of objects
+
+let bouquets: Array<typeof bouquet> = [];
+
+// pushing multipul objest
+
+bouquets.push(bouquet);
+
+// pushing multipul object
+
+bouquets.push(bouquet);
+bouquets.push({
+    name: "Summer bliss",
+    price: 350,
+    description: "Beautiful arrangement of summer flowers"
+})
+
+let bouquet3 = 
+{
+    name: "Red Hot",
+    price: 350,
+    description: "beautiful arrangement of red roses"
+}
+
+bouquets.push(bouquet3);
+
+// console.log(bouquets);
+
+bouquets.push({name: "Freshness", price:250, description: "A beautiful arrangement of white and yellow roses"});
+
+// console.log(bouquets);
+
+// function to display bouquets.
+
+function displayBouquets(bouquets: Array<typeof bouquet>)
+{
+    for(let i of bouquets) {
+        console.log(`
+        title: ${i.name}
+        description:${i.description}
+        price:${i.price}
+        ______________\n`);
+    }
+
+}
+
+displayBouquets(bouquets);
